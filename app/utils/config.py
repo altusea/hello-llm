0fdb8b8930933e46dd9b0d3562f9529202bc38d2
altusea <zt.song@outlook.com>
2025-02-06 13:51:53 +0800

    read .env file with lib 'python-dotenv'

--- app/utils/config.py
+++ app/utils/config.py
@@ -1,15 +1,19 @@
 import os
+from typing import Dict, Optional
+
+from dotenv import dotenv_values
 
 
 def read_api_key() -> str | None:
     try:
-        with open(os.path.expanduser("~/.env"), "r") as f:
-            for line in f:
-                line = line.strip()
-                if line.startswith("DEEPSEEK_API_KEY="):
-                    return line.split("=", 1)[1]
-            print("Error: DEEPSEEK_API_KEY not found in .env file.")
-            return None
+        kv_dict: Dict[str,Optional[str]] = dotenv_values(os.path.expanduser( "~/.env"))
+        print(kv_dict.keys)
+
+        v = kv_dict["DEEPSEEK_API_KEY"]
+        if v is not None:
+            return v
+        print("Error: DEEPSEEK_API_KEY not found in .env file.")
+        return None
     except FileNotFoundError:
         print("Error: .env file not found. Please create a .env file with DEEPSEEK_API_KEY.")
         return None
@@ -17,3 +21,8 @@ def read_api_key() -> str | None:
 
 def write_api_key_to_env(api_key: str):
     os.environ["DEEPSEEK_API_KEY"] = api_key
+
+if __name__ == "__main__":
+    kv_dict: Dict[str,Optional[str]] = dotenv_values(os.path.expanduser( "~/.env"))
+    for k, v in kv_dict.items():
+        print(f"{k}: {v}")
